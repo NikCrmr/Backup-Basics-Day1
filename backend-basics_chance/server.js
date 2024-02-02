8c5d4a86822a5f0253b5a91d8d03cf8af0dee96c
Niko Cremer <n.ko@posteo.de>
2024-02-02 12:14:27 +0100

    update

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -5,8 +5,14 @@ export const server = createServer((request, response) => {
   const chance = new Chance();
   const name = chance.name();
   const age = chance.age();
+  const character = chance.character();
+  const string = chance.string({ length: 22 });
+
   const profession = chance.profession();
 
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify({ name, age, profession }));
+  // response.writeHead(200, { "Content-Type": "application/json" });
+  // response.end(JSON.stringify({ name, age, profession, character, string }));
+  response.end(
+    `hello my name is ${name}, my age is ${age}, my profession: ${profession}, my favorite character is  ${character} and my password - Psst! Please keep it secret! ${string}}`
+  );
 });
